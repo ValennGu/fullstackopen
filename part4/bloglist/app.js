22bcc7e778c90247b198c47490f68ba99d6ba7b9
Valen Gutierrez <valentin.g.l@gmail.com>
2025-03-26 17:12:42 +0100

    split app and server logic

---
+++ part4/bloglist/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const { MONGODB_URI } = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+logger.info('Connecting to database ...')
+mongoose.connect(MONGODB_URI)
+  .then(() => logger.info('Connected to database'))
+  .catch(() => logger.error('Could not connect to database'))
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
